cfb691c8e196a655b0cfbbea100f3e6aba88246e
Gregers Poulsen <gp@airborneinstruments.dk>
2024-06-13 19:50:35 +0200

    Change user id when $MIKTEX_UID is set

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 MIKTEX_UID=${MIKTEX_UID:-1000}
 MIKTEX_GID=${MIKTEX_GID:-${MIKTEX_UID}}
-if [ "$(id -u miktex)" != 1000 ]; then
+if [ "$MIKTEX_UID" != 1000 ]; then
     usermod -u ${MIKTEX_UID} miktex >/dev/null
     groupmod -g ${MIKTEX_GID} miktex
     chown -R miktex:miktex /var/lib/miktex
